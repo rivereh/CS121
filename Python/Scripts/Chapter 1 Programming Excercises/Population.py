086ba7db6d003af214033378d71bfd433cec48af
River <riverehill@gmail.com>
2018-01-16 17:28:24 -0800

    Tuesday class items added

---
+++ Python/Scripts/Chapter 1 Programming Excercises/Population.py
@@ -0,0 +1,21 @@
+"""
+    CS121 W18
+    POPULATION
+    RIVER HILL
+    PRINTS POPULATION FOR NEXT 5 YEARS
+    1/16/18
+    PYTHON 3.6.4
+"""
+
+currentPopulation = 312032486
+secondsInYear = 31536000
+birthsInYear = secondsInYear // 7
+deathsInYear = secondsInYear // 13
+immigrantsInYear = secondsInYear // 45
+
+for x in range(1, 6):
+    totalPopulationInYear = ((birthsInYear * x) + (immigrantsInYear * x) - (deathsInYear * x)) \
+                            + currentPopulation + (x - 1)
+    print(totalPopulationInYear)
+
+# Test case should output 314812582, 317592679, 320372776, 323152873, 325932970
---
+++ Python/Scripts/Chapter 1 Programming Excercises/quiz.py
@@ -0,0 +1,12 @@
+
+
+import turtle
+turtle.color("red")
+turtle.pendown()
+turtle.circle (20)
+turtle.circle (-20)
+turtle.circle (30)
+turtle.circle (-30)
+turtle.circle (40)
+turtle.circle (-40)
+turtle.done()
