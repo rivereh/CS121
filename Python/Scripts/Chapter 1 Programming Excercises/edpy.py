6e8cbe8fe6d1ce1bec6919b490fad4e5b92bb376
River <riverehill@gmail.com>
2018-01-18 14:02:05 -0800

    added draw cube and cirrcle

---
+++ Python/Scripts/Chapter 1 Programming Excercises/edpy.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.color("red")
+turtle.pendown()
+turtle.circle (20)
+turtle.circle (-20)
+turtle.circle (30)
+turtle.circle (-30)
+turtle.circle (40)
+turtle.circle (-40)
+turtle.done()
