c84a546673c8a8f161e9ee555b3dd7b995808473
River <riverehill@gmail.com>
2018-02-14 22:01:16 -0800

    4.46 draftt

--- Python/Scripts/Excercises/6.46.py
+++ Python/Scripts/Excercises/6.46.py
@@ -1,1 +1,21 @@
-// clap controlled edpy
+import turtle
+
+turnAmount = 360 / 6
+
+points = []
+
+for x in range(6):
+    points.append(turtle.position())
+    turtle.forward(100)
+    turtle.right(turnAmount)
+
+for x in range(4):
+    for y in range(len(points)):
+        turtle.penup()
+        turtle.goto(points[x])
+        turtle.pendown()
+        turtle.goto(points[y])
+
+turtle.done()
+
+# clap controlled edpy
